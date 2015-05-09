6aaccb7556594ec870679cc8ca892dfefdc69ae9
Art Wild <wildart@gmail.com>
2015-05-09 19:43:47 -0400

    added `with_libgit2` helper function better memory resource handling `finalize` instead of `free`

--- base/pkg/dir.jl
+++ base/pkg/dir.jl
@@ -1,7 +1,7 @@
 module Dir
 
 import ..Pkg: DEFAULT_META, META_BRANCH
-import ..LibGit2
+import ..LibGit2, ..LibGit2.with_libgit2
 
 const DIR_NAME = ".julia"
 
@@ -42,11 +42,8 @@ function init(meta::AbstractString=DEFAULT_META, branch::AbstractString=META_BRA
         mkpath(dir)
         Base.cd(dir) do
             info("Cloning METADATA from $meta")
-            metadata_repo = LibGit2.clone(meta, "METADATA", branch = branch)
-            try
+            with_libgit2(LibGit2.clone(meta, "METADATA", branch = branch)) do metadata_repo
                 LibGit2.set_remote_url(metadata_repo, meta)
-            finally
-                LibGit2.free!(metadata_repo)
             end
             touch("REQUIRE")
             touch("META_BRANCH")
