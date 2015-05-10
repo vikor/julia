0a571a1903b67133e82c368e38c02a2737c9f7ef
Tim Holy <tim.holy@gmail.com>
2015-05-10 05:35:16 -0500

    Update typejoin for new Varargs
    This also turns debugging output back on in preparation for the final implementation
    The cmdlineargs test fails, but only due to the extra output generated.

--- src/jltypes.c
+++ src/jltypes.c
@@ -3283,7 +3283,6 @@ static int jl_subtype_le(jl_value_t *a, jl_value_t *b, int ta, int invariant)
     else
         newb = b;
     int retnew = jl_subtype_le_new(newa, newb, ta, invariant);
-    /*
     if (retnew != retold) {
         if (warnonce_subtype(ta, invariant, jl_object_id(a), jl_object_id(b))) {
             jl_printf(JL_STDOUT, "Disagree jl_subtype_le with retnew = %d, retold = %d (ta = %d, invariant = %d):\n", retnew, retold, ta, invariant);
@@ -3293,10 +3292,8 @@ static int jl_subtype_le(jl_value_t *a, jl_value_t *b, int ta, int invariant)
             jlt_(b);
         }
     }
-    */
     JL_GC_POP();
     return retnew;
-    //return retold;
 }
 
 int jl_subtype(jl_value_t *a, jl_value_t *b, int ta)
